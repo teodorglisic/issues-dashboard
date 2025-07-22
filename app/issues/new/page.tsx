ace25328cb61f3a5b38da3631d081f353e950cac
teodorglisic <teodor.glisic13@icloud.com>
2025-07-22 08:33:46 +0200

    Add a markdown editor

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { Button, TextField } from '@radix-ui/themes'
+import React from 'react'
+import SimpleMdeReact from 'react-simplemde-editor'
+import "easymde/dist/easymde.min.css";
+
+const NewIssuePage = () => {
+  return (
+    <div className='max-w-xl space-y-3'>
+        <TextField.Root placeholder='Title'></TextField.Root>
+
+        <SimpleMdeReact />
+        <Button>Submit New Issue</Button>
+
+    </div>
+  )
+}
+
+export default NewIssuePage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import './theme-config.css'
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,13 +27,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${inter.variable} antialiased`}
       >
-        <NavBar />
-
-        <main>
-          {children}
-        </main>
+        <Theme accentColor="violet">
+          <NavBar />
+           {/* <ThemePanel /> */}
+          <main className="p-5">
+            {children}
+          </main>
+        </Theme>
       </body>
     </html>
   );
